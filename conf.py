98748ac675aa4b4eff07461fa0da90d859bbf6a7
David Fraser <davidf@sjsoft.com>
2013-10-05 18:16:47 +0200

    Fix up html title

--- conf.py
+++ conf.py
@@ -103,7 +103,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "<project> v<release>"
+html_title = "%s v%s" % (project, release)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
