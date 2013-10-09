8687fc88f74689fdfed36f2ddaacc44e5a99ea44
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-09 14:15:26 +0200

    Rm docstring

--- clipbit/secretcodes.py
+++ clipbit/secretcodes.py
@@ -1,17 +1,3 @@
-"""
-Secret Codes
-============
-
-You can use this program to send secret messages to a friend who also has it,
-and they can use it to decode the messages so they can understand them.
-
-To get this to work, enter a message and then a number between ``1`` and ``25``
-(like ``3``).
-You will then receive a secret code for that message!
-To decode the message, you need to put in the secret code you got out,
-and then make the number a *minus* the first number you put in (like ``-3``).
-"""
-
 def off(c):
     if c.isalpha():
         return (ord(c) - ord('A') if c.isupper() else ord(c) - ord('a'))
