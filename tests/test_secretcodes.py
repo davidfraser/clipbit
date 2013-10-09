97b9901161011068372274a849190a98bf0b40db
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-09 14:19:08 +0200

    Rename tests

--- tests/test_secretcodes.py
+++ tests/test_secretcodes.py
@@ -3,14 +3,21 @@ from unittest2 import TestCase
 from clipbit import secretcodes
 
 
-class RotTestCase(TestCase):
-    def test_rot(self):
+class SecretCodesTestCase(TestCase):
+
+    def test_shift(self):
+        self.assertEquals(
+            '',
+            secretcodes.shift(1,2,3)
+        )
+
+    def test_encode(self):
         self.assertEquals(
             'ifmmp', 
             secretcodes.encode('hello', 1)
         )
 
-    def test_rot_decode(self):
+    def test_decode(self):
         self.assertEquals(
             'hello',
             secretcodes.decode('ifmmp', 1)
